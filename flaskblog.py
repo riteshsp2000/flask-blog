e54b2c13917698213f5e28000ecb25887695414d
Ritesh Patil <riteshsp2000@gmail.com>
2020-06-05 16:40:10 +0530

    Added the basic layout with home and about page

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+  {
+    "title": "Hi",
+    "author": "Ritesh",
+    "content": "Starting with flask",
+    "date_posted": "6/6/20"
+  }
+]
 
 @app.route("/")
 @app.route("/home")
 def home(): 
-  return render_template('home.html') 
+  return render_template('home.html', posts) 
 
 @app.route("/about")
 def about(): 
   return render_template('about.html')  
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True) 
